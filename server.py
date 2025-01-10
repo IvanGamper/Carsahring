e74e217ea13b936292e4637a93e17699f7aea931
IvanGamper <ivagamper1991@gmail.com>
2025-01-10 12:34:09 +0100

    1

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app = Flask(__name__, template_folder=template_dir)
 # Route für die Startseite
 @app.route('/')
 def home():
-    return render_template('index.html')  # Lädt die index.html im templates-Ordner
+    return render_template('index.html')
 
 @app.route('/cars', methods=['GET'])
 def get_cars():
